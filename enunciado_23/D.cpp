cba3adacd9946a7a1f6e4c8dbc59ff13a2ebdac9
Gabriel Coelho Costa <coelhocostag@gmail.com>
2024-10-19 14:40:26 -0300

    add metodo MD3() para class D

--- enunciado_23/D.cpp
+++ enunciado_23/D.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
 class D{
     private:
         int D1, D2;
     public:
         void MD1(){};
         void MD2(){};
+        void MD3();
 };
+
+void D::MD3(){
+    std::cout << "MD3";
+}
