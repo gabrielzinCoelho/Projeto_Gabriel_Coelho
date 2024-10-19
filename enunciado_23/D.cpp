e4df33d66d0400a106c7f33b2c0ead686bf95ba0
Gabriel Coelho Costa <56321159+gabrielzinCoelho@users.noreply.github.com>
2024-10-19 14:43:05 -0300

    Update D.cpp
    add metodo MD4() para class D

--- enunciado_23/D.cpp
+++ enunciado_23/D.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
 class D{
     private:
         int D1, D2;
     public:
         void MD1(){};
         void MD2(){};
+        void MD4();
 };
+
+void D::MD4(){
+    std::cout << "MD4";
+}
